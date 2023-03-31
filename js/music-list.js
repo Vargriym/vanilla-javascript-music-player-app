725b2010499910befd189d891e232ea1fe3e7977
Muhammad <102037554+Vargriym@users.noreply.github.com>
2023-03-31 20:10:02 +0200

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+    {
+      name: "Ode To The Mets",
+      artist: "The Strokes",
+      img: "Images/1.jpeg",
+      src: "Songs/1.mp3",
+    },
+    {
+      name: "Get Lucky",
+      artist: "Daft Punk",
+      img: "Images/2.jpeg",
+      src: "Songs/2.mp3",
+    },
+    {
+      name: "Last Night I Dreamt That Somebody Loved Me",
+      artist: "The Smiths",
+      img: "Images/3.jpeg",
+      src: "Songs/3.mp3",
+    },
+    {
+      name: "El 3asal",
+      artist: "Donia Wael",
+      img: "Images/4.jpeg",
+      src: "Songs/4.mp3",
+    },
+  ];
+  
